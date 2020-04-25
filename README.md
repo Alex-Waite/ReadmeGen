85c5f1fd6a3048538ef632bebaab8d70c0336630
Alex Waite <alexanderconorwaite@gmail.com>
2020-04-26 03:20:42 +0930

    Scrapped export idea basic app SEEMS to be working?

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-# ReadmeGen
-A program that creates readme files for future projects.
 
-# TEST STUFF!
-
-[![GitHub Follow](https://img.shields.io/github/followers/alex-waite?label=Follow%20me%20on%20GitHub%21&style=social)](https://github.com/Alex-Waite)
+  # PROJECT TITLE: alex
+  helol
+  
+  
--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const fs = require("fs");
 const util = require("util");
 
 // Import tag for retrieveing README.md content after data is applied
-import {
-    generateMarkdown
-} from "utils/generateMarkdown.js"
 
 // Function used to transfer the users data to the README.md file
 const writeFileAsync = util.promisify(fs.writeFile);
@@ -15,17 +12,17 @@ const writeFileAsync = util.promisify(fs.writeFile);
 const questions = [{
         type: "input",
         name: "title",
-        message: "What is your project called?"
+        message: "Enter a descriptive title for your project."
     },
     {
         type: "input",
         name: "description",
-        message: "Write a brief description for your project"
+        message: "Write a description for your project."
     },
     {
         type: "input",
         name: "installation",
-        message: "How is this application installed? (If inapplicable leave blank)"
+        message: "How is this application installed? (If inapplicable leave empty)"
     },
     {
         type: "input",
@@ -35,22 +32,22 @@ const questions = [{
     {
         type: "input",
         name: "contributions",
-        message: "Any notable contributions? If so who/how"
+        message: "Any notable contributions? If so who/how (If inapplicable leave empty)"
     },
     {
         type: "input",
         name: "linkedin",
-        message: "Enter your LinkedIn URL."
+        message: "Enter your LinkedIn URL. (If inapplicable leave empty)"
     },
     {
         type: "input",
         name: "github",
-        message: "Enter your GitHub Username"
+        message: "Enter your GitHub Username. (If inapplicable leave empty)"
     },
     {
         type: "input",
         name: "email",
-        message: "Enter your GitHub Email"
+        message: "Enter your GitHub Email. (If inapplicable leave empty)"
     },
 ];
 
@@ -59,10 +56,18 @@ function init() {
     return inquirer.prompt(questions)
 }
 
+function generateMarkdown(questions) {
+    return `
+  # PROJECT TITLE: ${questions.title}
+  ${questions.description}
+  
+  `;
+}
+
 // The workings to add data into a readme template and apply the template to the users README.md file
 init()
-    .then(function (data) {
-        const readMe = generateMarkdown(data);
+    .then(function (questions) {
+        const readMe = generateMarkdown(questions);
         return writeFileAsync("README.md", readMe);
     })
     .then(function () {
