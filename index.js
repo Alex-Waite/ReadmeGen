afad788d9a3cc7948580dd0fec6ffeeabc432d38
Alex Waite <alexanderconorwaite@gmail.com>
2020-04-26 02:26:39 +0930

    finished the init function? import markdown working? added code comments

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+// Node Module declarations
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
 
+// Import tag for retrieveing README.md content after data is applied
+import {
+    generateMarkdown
+} from "utils/generateMarkdown.js"
+
+// Function used to transfer the users data to the README.md file
 const writeFileAsync = util.promisify(fs.writeFile);
 
+// Inquirer questions to obtain content for the README.md file
 const questions = [{
         type: "input",
         name: "title",
@@ -46,10 +54,28 @@ const questions = [{
     },
 ];
 
-// function writeToFile(fileName, data) {}
-
+// Function to prompt the questions and begin the readme generation
 function init() {
     return inquirer.prompt(questions)
 }
 
-init();
+// The workings to add data into a readme template and apply the template to the users README.md file
+init()
+    .then(function (data) {
+        const readMe = generateMarkdown(data);
+        return writeFileAsync("README.md", readMe);
+    })
+    .then(function () {
+        console.log("Successfully wrote to README.md");
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
+
+
+
+
+
+// !!!!STUFF I HAVE COMMENTED OUT IN CODE!!! //
+
+// function writeToFile("README.md", data) {}
